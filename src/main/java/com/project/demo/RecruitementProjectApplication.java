814b5bb412fe3b9be3e09f8008b77be3c38fd0b8
thweookhine <mathweookhine@gmail.com>
2023-02-10 11:02:13 +0630

    first commit

---
+++ src/main/java/com/project/demo/RecruitementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitementProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/demo/RecruitementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
