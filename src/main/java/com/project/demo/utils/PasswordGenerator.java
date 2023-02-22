085ff6adb95e04e679df2400dffab58513d371fb
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2023-02-22 14:43:05 +0630

    main method in password generator

--- src/main/java/com/project/demo/utils/PasswordGenerator.java
+++ src/main/java/com/project/demo/utils/PasswordGenerator.java
@@ -9,4 +9,7 @@ public class PasswordGenerator {
 		return encoder.encode(password);
 	}
 
+	public static void main(String[] args) {
+		System.out.println("password - " + generatePassword("password"));
+	}
 }
