a8574beec5ce4e40df7c7c5cf358d91fc0252e34
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2023-02-23 14:27:54 +0630

    User Remodel
    -added Css and javascript
    -added a new method in Security.config
    -added a logout in home,html
    -added a nav
    -added components folder

---
+++ src/main/java/com/project/demo/controller/AjaxController.java
@@ -0,0 +1,20 @@
+package com.project.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AjaxController {
+
+}
+
+
+
+
+
+
+
+
+
--- src/main/java/com/project/demo/securityConfig/SpringSecurityConfig.java
+++ src/main/java/com/project/demo/securityConfig/SpringSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,58 +18,65 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	AuthenticationFailureHandler authenticationFailureHandler;
-	
+
 	@Autowired
 	private DataSource dataSource;
-	
+
 	@Bean
 	// Method
-	public PasswordEncoder passwordEncoder()
-	{
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**")
+		.antMatchers("/static/**")
+		.antMatchers("/css/**")
+		.antMatchers("/js/**");
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
+
 		auth.jdbcAuthentication()
-			.passwordEncoder(passwordEncoder())
-			.dataSource(dataSource)
-			.usersByUsernameQuery("select email, password, enabled from user where email = ?")
-			.authoritiesByUsernameQuery("select email, role from user where email = ?");
-		
-		}
-	
+				.passwordEncoder(passwordEncoder())
+				.dataSource(dataSource)
+				.usersByUsernameQuery("select email, password, enabled from user where email = ?")
+				.authoritiesByUsernameQuery("select email, role from user where email = ?");
+
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
 		http.csrf().disable();
-		
+
 		http.authorizeRequests()
-			.antMatchers("/admin/**").hasAuthority("ADMIN")
-			.antMatchers("/market/**").hasAnyAuthority("MARKETER","ADMIN")
-			.antMatchers("/login","/resources/**").permitAll()
-			.antMatchers("/**").hasAnyAuthority("ADMIN", "USER", "MARKETER")
-			.antMatchers("/home/**")
-			.authenticated();
-		
+				.antMatchers("/admin/**").hasAuthority("ADMIN")
+				.antMatchers("/market/**").hasAnyAuthority("MARKETER", "ADMIN")
+				.antMatchers("/login", "/resources/**").permitAll()
+				.antMatchers("/**").hasAnyAuthority("ADMIN", "USER", "MARKETER")
+				.antMatchers("/home/**")
+				.authenticated();
+
 		http.formLogin()
-			.loginPage("/login")
-			.defaultSuccessUrl("/home/homePage")
-			.loginProcessingUrl("/j_spring_security_check")
-			.usernameParameter("email")
-			.passwordParameter("password")
-			.failureHandler(authenticationFailureHandler);
-		
+				.loginPage("/login")
+				.defaultSuccessUrl("/home/homePage")
+				.loginProcessingUrl("/j_spring_security_check")
+				.usernameParameter("email")
+				.passwordParameter("password")
+				.failureHandler(authenticationFailureHandler);
+
 		http.logout()
-			.logoutUrl("/logout")
-			.invalidateHttpSession(true)
-			.logoutSuccessUrl("/login");
-		
+				.logoutUrl("/logout")
+				.invalidateHttpSession(true)
+				.logoutSuccessUrl("/login");
+
 		http.exceptionHandling()
-		    .accessDeniedPage("/unauthorized");
+				.accessDeniedPage("/unauthorized");
 	}
 }
